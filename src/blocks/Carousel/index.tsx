1931df14062caacf07dce08416df09b658a0a374
Abel Gonzalez <abelgonzalezdev@pm.me>
2025-10-04 16:08:12 +0000

    Cleanup code

--- src/blocks/Carousel/index.tsx
+++ src/blocks/Carousel/index.tsx
@@ -11,22 +11,6 @@ import {
 import Image from 'next/image'
 
 export const Carousel: ({ medias, id }: CarouselBlock) => ReactNode = ({ medias, id }) => {
-  // const getPrevIndex = (currentIndex: number, arrayLength: number) => {
-  //   if (currentIndex === 0) {
-  //     return arrayLength - 1
-  //   } else {
-  //     return currentIndex - 1
-  //   }
-  // }
-
-  // const getForwardIndex = (currentIndex: number, arrayLength: number) => {
-  //   if (currentIndex === arrayLength - 1) {
-  //     return 0
-  //   } else {
-  //     return currentIndex + 1
-  //   }
-  // }
-
   return (
     <CarouselComponent className="mx-15 ">
       <CarouselContent>
@@ -53,21 +37,5 @@ export const Carousel: ({ medias, id }: CarouselBlock) => ReactNode = ({ medias,
       <CarouselPrevious />
       <CarouselNext />
     </CarouselComponent>
-    // <div className="carousel w-full h-full">
-    //   {medias?.map((media, index) => (
-    //     <div key={media.id} id={`slide${index}`} className="carousel-item relative w-full">
-    //       <img src={media?.media?.url} className="w-full object-cover" />
-    //       <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-    //         <a
-    //           className="btn btn-circle"
-    //           href={`#slide${getPrevIndex(index, medias.length)}`}
-    //         >{`<`}</a>
-    //         <a className="btn btn-circle" href={`#slide${getForwardIndex(index, medias.length)}`}>
-    //           {'>'}
-    //         </a>
-    //       </div>
-    //     </div>
-    //   ))}
-    // </div>
   )
 }
